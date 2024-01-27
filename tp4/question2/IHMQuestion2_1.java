8b9c0fc6d03c1bbfbf3908fef49d757cf03439ea
Maroun Melhem <maroun.melhem@speedlane.co>
2024-01-27 18:07:13 +0200

    feat: finish question 2 part 1

--- tp4/question2/IHMQuestion2_1.java
+++ tp4/question2/IHMQuestion2_1.java
@@ -12,7 +12,7 @@ public class IHMQuestion2_1 extends JFrame {
 
     private TextArea contenu = new TextArea(30, 80);
 
- 
+
     public IHMQuestion2_1() {
         super("IHM Question2_1");
         JPanel enHaut = new JPanel();
@@ -21,22 +21,29 @@ public class IHMQuestion2_1 extends JFrame {
         enHaut.add(boutonC);
         setLayout(new BorderLayout(5, 5));
         add("North", enHaut);
-        add("Center", contenu); // contenu sera transmis aux observateurs, voir
-                                // la description des constructeurs
+        add("Center", contenu);
         enHaut.setBackground(Color.blue);
         setLocation(100,100);
         pack();show();
 
-        // à compléter
-        // le bouton A a 3 observateurs jbo1, jbo2 et jbo3
+         // Creation des observateurs.
+        JButtonObserver jbo1 = new JButtonObserver("jbo1", contenu);
+        JButtonObserver jbo2 = new JButtonObserver("jbo2", contenu);
+        JButtonObserver jbo3 = new JButtonObserver("jbo3", contenu);
 
-        // le bouton B a 2 observateurs jbo1 et jbo2
+        // Ajout aux boutons.
+        boutonA.addActionListener(jbo1);
+        boutonA.addActionListener(jbo2);
+        boutonA.addActionListener(jbo3);
 
-        // le bouton C a 1 observateur jbo1
+        boutonB.addActionListener(jbo1);
+        boutonB.addActionListener(jbo2);
 
-      
+        boutonC.addActionListener(jbo1);
+
+
     }
-    
+
     public static void main(String[] args){
         new IHMQuestion2_1();
     }
